3d6a327da9b9f28bb3a7faa805d07e43d5504274
Denhez Alexis <denhez.alexis@gmail.com>
2025-11-27 11:58:27 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const db = require('./config/database');
+db.query('SELECT 1')
+    .then(() => console.log('✓ Base de données MySQL connectée'))
+    .catch(err => console.error('✗ Erreur de connexion à la base:', err.message));
+
+const toolsRoutes = require('./routes/tools');
+app.use('/api/tools', toolsRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenue sur l\'API REST !' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
