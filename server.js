93731aaded0990de97b69f45f58939688f1ecc50
Denhez Alexis <denhez.alexis@gmail.com>
2025-11-28 14:45:33 +0100

    Add swagger api docs

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ const toolsRoutes = require('./routes/tools');
 const db = require('./config/database');
 const app = express();
 const PORT = 3000;
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: "3.0.4",
+        info: {
+            title: "Internal Tools API",
+            version: "1.0.0",
+            description: "Documentation rapide de l'API",
+        },
+        tags: [
+            {
+                name: "Tools",
+                description: "Gestion des outils"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"],
+}
+const specs = swaggerDoc(options);
 
 app.use(express.json());
 
@@ -11,6 +32,7 @@ db.query('SELECT 1')
     .catch(err => console.error('✗ Erreur de connexion à la base:', err.message));
 
 app.use('/api/tools', toolsRoutes);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Bienvenue sur l\'API REST !' });
